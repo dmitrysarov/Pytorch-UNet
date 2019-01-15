2e287e16227de7abf9e5e5af0dd42b8b0647a583
dmitrysarov <dmvish@yandex.ru>
2019-01-15 18:49:20 +0300

    remove backward in dice

--- dice_loss.py
+++ dice_loss.py
@@ -1,36 +1,50 @@
 import torch
 from torch.autograd import Function, Variable
 
-class DiceCoeff(Function):
-    """Dice coeff for individual examples"""
+def soft_dice_coef_batch_voluem(input, target):
+    '''
+    consider batch as voluem, and compute dice across whole batch samples as one,
+    but class wise, after that per class dices average performed.
+    '''
+    smooth = 0.0001
+    assert len(input.size()) == 4 #batch, class, h, w
+    assert len(target.size()) == 4
+    class_num = target.size(1)
+    input = input.transpose(0, 1).contiguous() #flip batch and classes dimentions
+    target = target.transpose(0, 1).contiguous()
+    input_flat = input.view(class_num, -1)
+    target_flat = target.view(class_num, -1)
+    inter = torch.sum(input_flat * target_flat, -1)
+    union = input_flat.sum(-1) + target_flat.sum(-1)
+    assert union.size(0) == class_num
+    assert inter.size(0) == class_num
+    soft_dice = torch.mean((2*inter + smooth) / (union + smooth)) #mean acros classes
+    return soft_dice
 
-    def forward(self, input, target):
-        self.save_for_backward(input, target)
-        eps = 0.00001
-        self.inter = torch.dot(input.view(-1), target.view(-1))
-        self.union = torch.sum(input) + torch.sum(target) + eps
-
-        t = (2 * self.inter.float() + eps) / self.union.float()
-        return t
-
-    # This function has only a single output, so it gets only one gradient
-    def backward(self, grad_output):
-
-        input, target = self.saved_variables
-        grad_input = grad_target = None
-
-        if self.needs_input_grad[0]:
-            grad_input = grad_output * 2 * (target * self.union - self.inter) \
-                         / (self.union * self.union)
-        if self.needs_input_grad[1]:
-            grad_target = None
-
-        return grad_input, grad_target
+def dice_loss(input, target):
+    soft_dice = soft_dice_coef_batch_voluem(input, target)
+    return -2 * soft_dice
 
 
-def dice_coeff(input, target):
-    """Dice coeff for batches"""
-    if torch.all(input == 0) and torch.all(target == 0):
-        return None
-    s = DiceCoeff().forward(input, target)
-    return s
+def dice_coef_for_val(input, target):
+    """Dice coeffs (class wise) for evaluation, should be implemented
+    subject wise. Batch should contain only one subject. Because of huge batch size
+    computation performed on cpu"""
+    assert len(input.size()) == 4
+    assert len(target.size()) == 4
+    input = input.transpose(0, 1).contiguous() #flip batch and classes dimentions
+    target = target.transpose(0, 1).contiguous()
+    dices = []
+    for i, t in zip(input, target):
+        if torch.all(i == 0) and torch.all(t == 0):
+        #prediction and target is empty
+            dices.append(-1)
+            continue
+        input_flat = i.view(-1)
+        target_flat = t.view(-1)
+        inter = torch.sum(input_flat * target_flat)
+        union = input_flat.sum() + target_flat.sum()
+        assert union != 0
+        dice = 2*inter/union
+        dices.append(dice.item())
+    return dices
